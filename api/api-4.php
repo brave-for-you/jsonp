b2fcb962968ba42ee1a633ef176eda36a8d7aeb4
sakura <1275292439@qq.com>
2017-07-01 15:11:33 +0800

    07/01-1511

--- api/api-4.php
+++ api/api-4.php
@@ -1,28 +1,26 @@
 <?php
 
-    header("Content-Type:application/json");
-        $name = $_POST['name'];
-        $birthday_year = $_POST['birthday_year'];
-        $height = $_POST['height'];
-        $weight = $_POST['weight'];
-        $mobile = $_POST['mobile'];
-        $glucos_value = $_POST['glucos_value'];
-        $a1c_value = $_POST['a1c_value'];
-        $diabetes_id = $_POST['diabetes_id'];
-        $meter_id = $_POST['meter_id'];
-        $doctor = $_POST['doctor'];
-        echo json_encode(
-                array(
-                    'name'=>$name,
-                    'height'=>$height,
-                    'weight'=>$weight,
-                    'mobile'=>$mobile,
-                    'glucos_value'=>$glucos_value,
-                    'a1c_value'=>$a1c_value,
-                    'diabetes_id'=>$diabetes_id,
-                    'meter_id'=>$meter_id,
-                    'doctor'=>$doctor,
-                    'birthday_year'=>$birthday_year,
-                )
-            );
+    header('Content-Type:text/html;charset=utf-8');
+
+    $con = mysql_connect("127.0.0.1","root","root@biohermes");
+
+    if (!$con){
+        die('Could not connect: ' . mysql_error());
+    }
+
+    mysql_select_db("hengshan", $con);
+
+    //sql 语句
+    $sql="INSERT INTO bio_offline_examiners (a1c_value,birthday_year,diabetes_id,doctor,glucos_value,height,meter_id,mobile,name,weight)
+
+    VALUES
+    ('$_POST[a1c_value]','$_POST[birthday_year]','$_POST[diabetes_id]','$_POST[doctor]','$_POST[glucos_value]','$_POST[height]','$_POST[meter_id]','$_POST[mobile]','$_POST[name]','$_POST[weight]')";
+    if (!mysql_query($sql,$con)){
+      die('Error: ' . mysql_error());
+    }
+
+    //添加成功
+    echo '{"status":"ok"}';
+    //关闭跟数据库的连接
+    mysql_close($con)
 ?>
